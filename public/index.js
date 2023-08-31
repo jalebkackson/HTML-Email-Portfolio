b2fe192c7f449dd9c1f1dc29b781b230e2c15384
Caleb Jackson <jalebgideonkackson@gmail.com>
2023-08-31 18:53:08 -0400

    added skills section and hacker animation/code

--- public/index.js
+++ public/index.js
@@ -52,3 +52,42 @@ const typewriter3 = () => {
   }
 };
 window.addEventListener("load", typewriter);
+
+//
+//
+//
+//
+// Hacker animation on text
+// const letters = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+// const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const letters = "abcdefghijklmnopqrstuvwxyz";
+
+let interval = null;
+
+document.querySelectorAll("#hacker").forEach((h1) => {
+  h1.onmouseover = (event) => {
+    let iteration = 0;
+
+    clearInterval(interval);
+
+    interval = setInterval(() => {
+      event.target.innerText = event.target.innerText
+        .split("")
+        .map((letter, index) => {
+          if (index < iteration) {
+            return event.target.dataset.value[index];
+          }
+
+          return letters[Math.floor(Math.random() * 26)];
+        })
+        .join("");
+
+      if (iteration >= event.target.dataset.value.length) {
+        console.log();
+        clearInterval(interval);
+      }
+
+      iteration += 1 / 6;
+    }, 15);
+  };
+});
